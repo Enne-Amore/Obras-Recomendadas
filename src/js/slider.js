2a581a5c1419501d444fc4c39511f97dbbe1e4db
”Enne” <EnneFofe10@gmail.com>
2023-11-29 17:11:39 -0300

    Indicações
    Adição de bolas indicadoras da ordem de obra que está sendo mostrada no momento.

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,10 +5,15 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slide");
-    let foto = document.getElementsByClassName("foto");
+    let bolinhas = document.getElementsByClassName("bolinha")
+    
     if (window.innerWidth < 600) {
       window.scrollTo({top: 110, behavior: 'smooth'});
     } else if (window.innerWidth < 1000) {
@@ -16,6 +21,7 @@ function showSlides(n) {
     } else if (window.innerWidth < 1200) {
       window.scrollTo({top: 160, behavior: 'smooth'});
     }
+
     if (n > slides.length) {
       slideIndex = 1
     }
@@ -24,6 +30,11 @@ function showSlides(n) {
     }
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
+    }
+    for (i = 0; i < bolinhas.length; i++) {
+      if (bolinhas[i].style.background = "rgba(0, 0, 0, 0.151)") {
+        bolinhas[slideIndex-1].style.background = "#23395b"
+      }
     }
     slides[slideIndex-1].style.display = "flex";
   }
