bbd77fe9f1c097eec9872602c274bc1f2ee59d61
”Enne” <EnneFofe10@gmail.com>
2023-12-23 16:02:31 -0300

    Bolinhas Indicadoras
    Conseguindo aplicar o efeito hover nas bolinhas indicadoras.

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# 🌸 Obras Recomendadas
+
+# 🎨 Obras Recomendadas
 
 Um site web que apresenta várias obras recomendadas a serem consumidas. Essa página se concentra no poder da JavaScript de fazer um slider funcionar.
+
+
+## ✅ Demonstração
+
+[Clique aqui para acessar o meu site](https://enne-amore.github.io/Obras-Recomendadas/)
+
+
+## 🚀 Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+
+## 🪄 Funcionalidades
+
+- Mais acessibilidade usando a tecla "tab"
+- Efeitos de "hover" e de "active" nos elementos clicáveis
+- Fazer desaparecer e aparecer informações conforme o slide vai sendo selecionado
+- Efeito de aparecimento suave aos slides
+
+## 🌈 Documentação de cores
+
+| Cor               | Hexadecimal                                                |
+| ----------------- | ---------------------------------------------------------------- |
+| Azul Clara       | ![#406E8E](https://via.placeholder.com/10/406E8E?text=+) #406E8E |
+| Azul Neutra       | ![#23395B](https://via.placeholder.com/10/23395B?text=+) #23395B |
+| Azul Escura       | ![#161925](https://via.placeholder.com/10/161925?text=+) #161925 |
+| Vermelha       | ![#F9627D](https://via.placeholder.com/10/F9627D?text=+) #F9627D |
+| Cinza       | ![#dbdbdb](https://via.placeholder.com/10/dbdbdb?text=+) #dbdbdb |
+
+
+## 🌟 Referências De Uso
+
+ - [Freepik](https://www.flaticon.com/br/autores/freepik)
+ - [www.flaticon.com'](https://www.flaticon.com/br/)
+ 
+
+## 🔧 Suporte
+
+Para me contatar como suporte, o meu email é EnneFofe10@gmail.com 
+
--- src/js/slider.js
+++ src/js/slider.js
@@ -10,31 +10,39 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    let bolinhas = document.getElementsByClassName("bolinha")
-    
-    if (window.innerWidth < 600) {
-      window.scrollTo({top: 110, behavior: 'smooth'});
-    } else if (window.innerWidth < 1000) {
-      window.scrollTo({top: 250, behavior: 'smooth'});
-    } else if (window.innerWidth < 1200) {
-      window.scrollTo({top: 160, behavior: 'smooth'});
-    }
-
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-      slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < bolinhas.length; i++) {
-      if (bolinhas[i].style.background = "rgba(0, 0, 0, 0.151)") {
-        bolinhas[slideIndex-1].style.background = "#23395b"
-      }
-    }
-    slides[slideIndex-1].style.display = "flex";
+  // Declaração de variáveis
+  let slides = document.querySelectorAll(".slide");
+  const bolinhas = document.querySelectorAll(".bolinha")
+  
+  // Para voltar ao começo da imagem em telas menores
+  if (window.innerWidth < 600) {
+    window.scrollTo({top: 110, behavior: 'smooth'});
+  } else if (window.innerWidth < 1000) {
+    window.scrollTo({top: 250, behavior: 'smooth'});
+  } else if (window.innerWidth < 1200) {
+    window.scrollTo({top: 160, behavior: 'smooth'});
   }
+
+  // Remove a classe 'aparecida' de todas as folhas
+  slides.forEach((slide) => {
+    slide.classList.remove('aparecida')
+  })
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  // Adiciona a classe 'aparecida' à folha correspondente
+  slides[slideIndex - 1].classList.add('aparecida')
+
+  // Remove a classe 'selecionada' de todas as bolinhas
+  bolinhas.forEach((bolinha) => {
+      bolinha.classList.remove('selecionada')
+  })
+
+  // Adiciona a classe 'selecionada' à bolinha correspondente
+  bolinhas[slideIndex - 1].classList.add('selecionada')
+}
